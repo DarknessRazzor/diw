7635211736d4a9f7d06a3c0fcaaee305fbbc12f6
daw <adrianoriolamartos1996@gmail.com>
2023-11-13 20:30:14 +0100

    Añadidas nuevas animaciones con javascript

---
+++ cursoDesplegable/animacion.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    let chincheta = document.getElementsByClassName("chincheta")[0];
+    chincheta.classList.add("moverChincheta1");
+    let chincheta2 = document.getElementsByClassName("chincheta")[1];
+    chincheta2.classList.add("moverChincheta2");
+    let chincheta3 = document.getElementsByClassName("chincheta")[2];
+    chincheta3.classList.add("moverChincheta3");
+
+
+    let telefonos = document.getElementsByTagName("article")[0];
+    telefonos.classList.add("article1");
+    let telefonos2 = document.getElementsByTagName("article")[2];
+    telefonos2.classList.add("article3");
+}
